bc3cdd64eaf6587c274be6af968d3489485f4197
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-25 08:25:07 +0500

    news

---
+++ src/components/news/VideoNovosti.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="py-9">
+    <div class="container">
+      <h2 class="text-5xl text-[#F7931F] font-medium py-3">Видео новости</h2>
+      <div class="md:grid grid-cols-2 gap-3">
+        <div class="md:flex flex-col row-span-2">
+          <img src="/newsA.png" alt="" />
+          <p class="text-2xl font-medium py-3">
+            Российская компания Wildberries планирует выход на рынок Узбекистана
+          </p>
+          <p class="text-[#666666]">
+            Стороны обсудили возможное сотрудничество в сфере экспортных
+            интернет-продаж текстильной и швейно-трикотажной промышленности с
+            производителями Узбекистана.
+          </p>
+          <div class="flex py-4">
+            <button class="px-4 py-2 bg-[#F7931F] text-white rounded">
+              Подробно
+            </button>
+          </div>
+        </div>
+        <div class="md:flex gap-3">
+          <div class="md:w-1/2">
+            <img src="/newsB.png" alt="" class="w-full h-full" />
+          </div>
+          <div class="md:w-1/2">
+            <p class="text-2xl font-medium">
+              В топ-10 российских онлайн-магазинов впервые вошла интернет-аптека
+            </p>
+            <p class="text-[#666666]">
+              OВ десятку крупнейших интернет-магазинов России по версии
+              аналитического агентства Data Insight впервые вошла онлайн-аптека
+              – apteka.ru. По итогам 2019 г. она поднялась с 13-го на 7-е место.
+            </p>
+            <div class="flex py-4 justify-between items-center">
+              <button class="px-4 py-2 bg-[#F7931F] text-white rounded">
+                Подробно
+              </button>
+              <p class="text-blue-400">2024-02-06</p>
+            </div>
+          </div>
+        </div>
+        <div class="md:flex gap-3">
+          <div class="md:w-1/2">
+            <img src="/newsC.png" alt="" class="h-full" />
+          </div>
+          <div class="md:w-1/2">
+            <p class="text-2xl font-medium">
+              Глава китайского Alibaba обсудил с Трампом создание рабочих мест в
+              США
+            </p>
+            <p class="text-[#666666]">
+              Стороны обсудили возможное сотрудничество в сфере экспортных
+              интернет-продаж текстильной и швейно-трикотажной промышленности с
+              производителями Узбекистана.
+            </p>
+            <div class="flex py-4 justify-between items-center">
+              <button class="px-4 py-2 bg-[#F7931F] text-white rounded">
+                Подробно
+              </button>
+              <p class="text-blue-400">2024-02-06</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/news/news.vue
+++ src/components/news/news.vue
@@ -1,15 +1,65 @@
 <script setup>
+import { ref } from "vue";
+import VideoNovosti from "./VideoNovosti.vue";
 
+const news = ref([
+  {
+    img: "/news.png",
+    title: "В AliExpress представили портрет типичного покупателя в регионах",
+    description:
+      "Петербуржцы закупаются пляжными сабо, омички — теплыми тапочками, а в Нижнем Новгороде заказывают жилеты с подогревом. ",
+    date: "2014-01-05",
+  },
+  {
+    img: "/news.png",
+    title: "В AliExpress представили портрет типичного покупателя в регионах",
+    description:
+      "Петербуржцы закупаются пляжными сабо, омички — теплыми тапочками, а в Нижнем Новгороде заказывают жилеты с подогревом. ",
+    date: "2014-01-05",
+  },
+  {
+    img: "/news.png",
+    title: "В AliExpress представили портрет типичного покупателя в регионах",
+    description:
+      "Петербуржцы закупаются пляжными сабо, омички — теплыми тапочками, а в Нижнем Новгороде заказывают жилеты с подогревом. ",
+    date: "2014-01-05",
+  },
+]);
 </script>
 
 <template>
-    <div>
-<h1>News</h1>
+  <div class="py-9">
+    <div class="container">
+      <div class="flex gap-3">
+        <p>Главная страница</p>
+        <p>/</p>
+        <p>Новости</p>
+      </div>
+      <h2 class="text-5xl text-[#F7931F] font-medium py-3">Новости</h2>
+      <div class="md:flex justify-between gap-5">
+        <div
+          v-for="(item, index) in news"
+          :key="index"
+          class="border shadow-xl"
+        >
+          <img :src="item.img" alt="" />
+          <div class="px-3">
+            <h3 class="text-2xl font-medium py-3">
+              {{ item.title }}
+            </h3>
+            <p class="text-[#666666]">{{ item.description }}</p>
+            <div class="flex justify-between py-4">
+              <button class="px-4 py-2 bg-[#F7931F] text-white rounded">
+                Подробно
+              </button>
+              <p class="text-blue-400">{{ item.date }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <VideoNovosti />
 </template>
 
-
-
-<style  scoped>
-
-</style>
+<style scoped></style>
